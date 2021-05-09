c77d206354889e6dee731e10cdfba91e2d447b39
Yenson <yenson.lau@gmail.com>
2021-05-09 15:02:11 -0400

    Finished fixing build.jl

--- build.jl
+++ build.jl
@@ -1,10 +1,28 @@
 using Pkg; Pkg.activate("."); Pkg.instantiate();
-using NodeJS; run(`$(npm_cmd()) install highlight.js`);
-using Franklin;
 import Franklin;
 
-rm("__site", recursive=true, force=true);
+if !Franklin.FD_CAN_HIGHLIGHT
+  # This whole bit exists simply because the constants in Franklin
+  # can't be updated even after highlight.js is installed
 
-Franklin.def_GLOBAL_VARS!();
-Franklin.convert_md(read("config.md", String); isconfig=true);
-optimize(prepath=Franklin.globvar(:prepath));
+  # e.g. /home/user/.julia
+  juliapath = pathof(Franklin);
+  juliapath = juliapath[1:findfirst("packages", juliapath)[1]-2];
+
+  Pkg.rm("Franklin");
+  rm("$(juliapath)/packages/Franklin", recursive=true, force=true);
+  rm("$(juliapath)/compiled/Franklin", recursive=true, force=true);
+
+  using NodeJS; run(`$(npm_cmd()) install highlight.js`)
+  Pkg.add("Franklin")
+  run(`julia build.jl`)
+
+else
+  # Once highlight.js is installed
+
+  rm("__site", recursive=true, force=true);
+
+  Franklin.def_GLOBAL_VARS!();
+  Franklin.convert_md(read("config.md", String); isconfig=true);
+  Franklin.optimize(prepath=Franklin.globvar(:prepath));
+end
