deff32e88b446d5d47328206f27093779be87b59
yenson-lau <yenson-lau@users.noreply.github.com>
2021-05-09 16:52:25 +0000

    Deploying to gh-pages from  @ 9ab3c0bc6f585586c4d3cae32d60308ddf47648f 🚀

--- build.jl
+++ build.jl
@@ -1,4 +1,6 @@
 using Pkg; Pkg.activate("."); Pkg.instantiate();
 using NodeJS; run(`$(npm_cmd()) install highlight.js`);
 using Franklin;
-optimize()
+
+rm("__site", recursive=true, force=true)
+optimize(prepath="minimal")
