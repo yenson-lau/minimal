d212a58c86ea3aeed99c50ccd9edd82815b25242
Yenson <yenson.lau@gmail.com>
2021-05-09 13:21:01 -0400

    Fixing deployment on GitHub

--- build.jl
+++ build.jl
@@ -1,6 +1,10 @@
 using Pkg; Pkg.activate("."); Pkg.instantiate();
 using NodeJS; run(`$(npm_cmd()) install highlight.js`);
 using Franklin;
+import Franklin;
 
-rm("__site", recursive=true, force=true)
-optimize(prepath="minimal")
+rm("__site", recursive=true, force=true);
+
+Franklin.def_GLOBAL_VARS!();
+Franklin.convert_md(read("config.md", String); isconfig=true);
+optimize(prepath=Franklin.globvar(:prepath));
